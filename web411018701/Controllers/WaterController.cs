638b9fc0ed677c231b47145c5672623e98cd0c33
MT knight <mark42967151@gmail.com>
2023-05-17 19:48:21 +0800

    water HW1

--- web411018701/Controllers/WaterController.cs
+++ web411018701/Controllers/WaterController.cs
@@ -11,6 +11,16 @@ namespace web411018701.Controllers
         // GET: Water
         public ActionResult Index(String name, float? weight)
         {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight.Value * 30) + "cc";
+            }
+
+            // 將數值傳給 view 顯示
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+
             return View();
         }
     }
