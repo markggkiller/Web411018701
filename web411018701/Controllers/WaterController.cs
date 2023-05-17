e09a8801f48368fa4d2e2d8db0d7e42ae564d525
MT knight <mark42967151@gmail.com>
2023-05-17 19:31:04 +0800

    water

---
+++ web411018701/Controllers/WaterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web411018701.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(String name, float? weight)
+        {
+            return View();
+        }
+    }
+}
